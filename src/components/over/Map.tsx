3ad57c23a2fd4805d7c2b3d366feba7e506852ff
n.tkachev <n.tkachev@skbkontur.ru>
2025-04-25 22:46:07 +0500

    feat (map): map WIP

--- src/components/over/Map.tsx
+++ src/components/over/Map.tsx
@@ -1,35 +1,50 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { load } from '@2gis/mapgl';
-import {environment} from "../../environments/environment.ts";
+import { environment } from '../../environments/environment.ts';
 
 const MapWrapper = React.memo(() => {
-    return <div id="map-wrapper" style={{ width: '100%', height: '90vh' }}/>
-}, () => true)
+    return <div id="map-wrapper" style={{ width: '100%', height: '90vh' }} />;
+}, () => true);
 
 export const Map: React.FC = () => {
     useEffect(() => {
         let map: any;
+        let marker: any;
         let isMounted = true;
 
         load().then((mapglAPI) => {
             if (!isMounted) return;
+
+            // Инициализация карты
             map = new mapglAPI.Map('map-wrapper', {
-                center: [55.31878, 25.23584],
-                zoom: 13,
+                center: [60.590549, 56.844352],
+                zoom: 16,
                 key: environment.apiKey,
+            });
+
+            map.on('styleload', () => {
+                marker = new mapglAPI.Marker(map, {
+                    coordinates: [60.590549, 56.844352],
+                    icon: 'https://docs.2gis.com/img/mapgl/marker.svg',
+                    label: {
+                        text: "ТУТ ХАКАТОН СУЧКИ!",
+                        offset: [0, 25],
+                        relativeAnchor: [0.5, 0],
+                    }
+                });
             });
         });
 
         return () => {
             isMounted = false;
+            if (marker) marker.destroy();
             if (map) map.destroy();
         };
     }, []);
 
-
     return (
         <div style={{ width: '100%', height: '100%' }}>
-            <MapWrapper/>
+            <MapWrapper />
         </div>
     );
 };
